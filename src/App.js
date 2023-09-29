07617ec35d2853444e6169be3eea7def426631d9
Saurav Sharma <sharma9310175627@gmail.com>
2023-09-29 23:33:55 +0530

    five

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="container" >
      <h1 style={{color:"red", fontWeight:"bold"}}>Meme Generator</h1>
      <Routes>
-    <Route path="sharma-1729.github.io/Meems-generator/" element={<Mhome/>} />
+    <Route path="/" element={<Mhome/>} />
     <Route path="/edit" element={<Medit/>}/>
      </Routes>
     </div>
